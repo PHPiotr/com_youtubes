55611d05bd7784b8aae9b25a778760ef571cd4b8
Piotr Kowalski <piet.kowalski@gmail.com>
2014-02-09 12:13:09 +0100

    Add duration and title to thumbs

--- site/views/youtubes/tmpl/default.php
+++ site/views/youtubes/tmpl/default.php
@@ -20,10 +20,27 @@ if (count($this->items) > 0):
 
 						<div class="jcarousel" style="width:<?php echo (count($this->items) === 2) ? '158px;' : ((count($this->items) === 3) ? '316px;' : '474px;') ?>">
 							<ul>
-								<?php foreach ($this->items as $key => $item): ?>
+								<?php
+								foreach ($this->items as $key => $item):
+									$YTurl = "http://gdata.youtube.com/feeds/api/videos/" . $this->escape($item->link);
+
+									$YTxml = simplexml_load_file($YTurl);
+									$YTtitle = $YTxml->title[0];
+									$YTduration = (int) $YTxml->children('media', true)->group[0]->children('yt', true)->duration[0]->attributes('', true)->seconds;
+									$YThour = '';
+									$YTmin = (int) ($YTduration / 60);
+									if ($YTmin > 59) {
+										$YThour = (int) ($YTmin / 60) . ':';
+										$YTmin = $YTmin % 60;
+									}
+									$YTs = $YTduration % 60;
+									$YTsec = $YTs < 9 ? "0" . $YTs : $YTs;
+									?>
 									<li>
 										<a class="thumb" id="<?php echo $this->escape($item->link); ?>" href="http://www.youtube.com/watch?v=<?php echo $this->escape($item->link); ?>" title="<?php echo $item->title; ?>">
 											<img title="<?php echo $item->title ?>" alt="<?php echo $item->title ?>" width="156" height="86" src='http://i1.ytimg.com/vi/<?php echo $this->escape($item->link); ?>/mqdefault.jpg' />
+											<span class="video title"><?php echo JHTML::_('string.truncate', ($item->title), 25); ?></span>
+											<span class="video time"><?php echo $YThour . $YTmin . ':' . $YTsec; ?></span>
 										</a>
 									</li>
 								<?php endforeach; ?>
@@ -80,6 +97,6 @@ if (count($this->items) > 0):
 			})(jQuery);
 
 		</script>
-	<?php
+		<?php
 	endif;
  endif;
